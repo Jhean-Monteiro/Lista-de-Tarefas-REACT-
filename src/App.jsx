790849528886f70c51094e42a1b56160aa4a545b
Jhean-Monteiro <jheanmonteiro.2019>
2026-01-17 15:00:09 -0300

    ver 0.1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+export function App() {
+
+    return (
+        <div>
+            <h1>Olá, Mundo!</h1>
+            <p>Bem-vindo ao meu aplicativo React.</p>
+        </div>
+    ); 
+}
+
+export default App;
