6562e71b5e41b1172c7a105933d92c8fa0bbdac8
Jhean-Monteiro <jheanmonteiro.2019>
2026-01-19 22:00:29 -0300

    ver 0.3

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
+
+// form
+import { FaPlus } from "react-icons/fa";
+
+// tarefas
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 import "./main.css";
 
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("ola mundo");
+
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+    if (novaTarefa === "") return;
+
+    const novasTarefas = [...tarefas];
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+      novaTarefa: "",
+    });
   };
 
   handleChange = (e) => {
@@ -11,20 +36,35 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
-        <h1>{novaTarefa}</h1>
+        <h1>Lista de Tarefas {novaTarefa}</h1>
 
-        <form action="#">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input
             onChange={this.handleChange}
             type="text"
             placeholder="Adicionar nova tarefa"
+            value={novaTarefa}
           />
-          <button type="submit">Adicionar</button>
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            <li key={index}>
+              {tarefa}
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
